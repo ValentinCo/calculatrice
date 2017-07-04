81fad680dd8506afeae1eeea96344447d43f64fa
valentinCo <eersel-val@hotmail.com>
2017-07-04 13:56:55 +0200

    Fin de la calculatrice

---
+++ calc.js
@@ -0,0 +1,9 @@
+var nombre1 =Number ( prompt ("Choisir un nombre"));
+var nombre2 =Number ( prompt("Choisir un deuxieme nombre"));
+
+var addition = nombre1 + nombre2;
+var multiplication = nombre1 * nombre2;
+
+alert(addition);
+alert(multiplication);
+
