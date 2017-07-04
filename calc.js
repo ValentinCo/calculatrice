1dae973cef3f2168cc2abaf3997171208db24474
valentinCo <eersel-val@hotmail.com>
2017-07-04 15:05:25 +0200

    function add et multi

--- calc.js
+++ calc.js
@@ -1,9 +1,39 @@
+
+	
+	
+
 var nombre1 =Number ( prompt ("Choisir un nombre"));
 var nombre2 =Number ( prompt("Choisir un deuxieme nombre"));
 
-var addition = nombre1 + nombre2;
+
+
+function addition(nombre1, nombre2)
+{
+	var result;
+	result = nombre1 + nombre2;
+	return result;
+
+}
+
+var resultat = addition(nombre1,nombre2);
+alert(resultat);
+
+
+
+function multiplication(nombre1, nombre2)
+{
+	var result;
+	result = nombre1 * nombre2;
+	return result;
+
+}
+
+var resultat = multiplication(nombre1,nombre2);
+alert(resultat);
+
+/*var addition = nombre1 + nombre2;
 var multiplication = nombre1 * nombre2;
 
 alert(addition);
 alert(multiplication);
-
+*/
